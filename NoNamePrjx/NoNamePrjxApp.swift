aef3f6ee6953ada73aba11153f5f91894b526e70
Ignat  Rogachevich <rssxzp@icloud.com>
2026-04-03 12:27:49 +0500

    initial commit

---
+++ NoNamePrjx/NoNamePrjxApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoNamePrjxApp.swift
+//  NoNamePrjx
+//
+//  Created by Игнат Рогачевич on 3.04.26.
+//
+
+import SwiftUI
+
+@main
+struct NoNamePrjxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
