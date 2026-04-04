a204a636db1d746b959bcfd2d79bd2a6d7ca5657
Ignat  Rogachevich <rssxzp@icloud.com>
2026-04-04 14:41:37 +0500

    feat: added gitignore

---
+++ NoNamePrjx/MovieDetails/MovieDetails.swift
@@ -0,0 +1,18 @@
+//
+//  MovieDetails.swift
+//  NoNamePrjx
+//
+//  Created by Игнат Рогачевич on 3.04.26.
+//
+
+import SwiftUI
+
+struct MovieDetails: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+#Preview {
+    MovieDetails()
+}
