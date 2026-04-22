0b76d105deabed56bda551c28921d5e483b442c7
Ignat  Rogachevich <rssxzp@icloud.com>
2026-04-23 03:06:18 +0500

    update gitignore

---
+++ NoNamePrjx/Filters/FilterScreen.swift
@@ -0,0 +1,18 @@
+//
+//  FilterScreen.swift
+//  NoNamePrjx
+//
+//  Created by Игнат Рогачевич on 13.04.26.
+//
+
+import SwiftUI
+
+struct FilterScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FilterScreen()
+}
---
+++ NoNamePrjx/Filters/ListCell.swift
@@ -0,0 +1,18 @@
+//
+//  ListCell.swift
+//  NoNamePrjx
+//
+//  Created by Игнат Рогачевич on 13.04.26.
+//
+
+import SwiftUI
+
+struct ListCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListCell()
+}
